323004eeda9343d507d1382fe36648fc06bf1e42
aashevtsov <work.shevcov@gmail.com>
2025-08-06 13:58:23 +0300

    add etherium

--- BTC_price.py
+++ BTC_price.py
@@ -1,12 +1,15 @@
 import requests
 
-def get_btc_price():
-    url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
+def get_crypto_price(crypto_id):
+    url = f"https://api.coingecko.com/api/v3/simple/price?ids={crypto_id}&vs_currencies=usd"
     response = requests.get(url)
     data = response.json()
-    btc_price = data['bitcoin']['usd']
-    return btc_price
+    crypto_price = data[crypto_id]['usd']
+    return crypto_price
 
 # Example usage:
-btc_price = get_btc_price()
+btc_price = get_crypto_price('bitcoin')
 print(f"The current price of Bitcoin is ${btc_price}")
+
+eth_price = get_crypto_price('ethereum')
+print(f"The current price of Ethereum is ${eth_price}")
